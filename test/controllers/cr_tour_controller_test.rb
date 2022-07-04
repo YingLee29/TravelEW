de748d3084b15d7dc1223dc786ff8afa0a6db642
lequan <lequan13576@gmail.com>
2022-07-04 11:41:03 +0700

    Update Gemfile.lock

--- test/controllers/cr_tour_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CrTourControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/booktour_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class BooktourTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
