d81e84a140be86c58686f58e04b15522b253448e
lequan <lequan13576@gmail.com>
2022-07-12 17:34:35 +0700

    login_social

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,23 +1,49 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-	def facebook
-    generic_callback :facebook
-  end
-
-  def generic_callback provider
-    @identity = User.from_omniauth(request.env["omniauth.auth"])
-
-    @user = @identity || current_user
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    binding.pry
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message :notice, :success, kind: provider.capitalize if
-        is_navigational_format?
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
-      session["devise.#{provider}_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra)
+      redirect_to root_path
     end
   end
-
+  def google_oauth2
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      if @user.persisted?
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect @user, event: :authentication
+      else
+        session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      end
+  end
   def failure
     redirect_to root_path
   end
+
+
+	# def facebook
+ #    generic_callback :facebook
+ #  end
+
+ #  def generic_callback provider
+ #    @identity = User.from_omniauth(request.env["omniauth.auth"])
+
+ #    @user = @identity || current_user
+ #    if @user.persisted?
+ #      sign_in_and_redirect @user, event: :authentication
+ #      set_flash_message :notice, :success, kind: provider.capitalize if
+ #        is_navigational_format?
+ #    else
+ #      session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+ #      redirect_to new_user_registration_url
+ #    end
+ #  end
+
+ #  def failure
+ #    redirect_to root_path
+ #  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,26 +5,33 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   validates :name, presence: true
-  validates :phone, presence: true
+  #validates :phone, presence: true
   enum role: {admin: 0, user: 1}
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name
-      user.image = auth.info.image
-      user.skip_confirmation!
-    end
+    name_split = auth.info.name.split(" ")
+    user = User.where(email: auth.info.email).first
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, name: name_split[0], email: auth.info.email, password: Devise.friendly_token[0, 20])
+      user
   end
-  def self.new_with_session params, session
-    super.tap do |user|
-      if data = session["devise.#{provider}_data"] &&
-        session["devise.#{provider}_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
+
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0,20]
+  #     user.name = auth.info.name
+  #     user.image = auth.info.image
+  #     user.skip_confirmation!
+  #   end
+  # end
+  # def self.new_with_session params, session
+  #   super.tap do |user|
+  #     if data = session["devise.#{provider}_data"] &&
+  #       session["devise.#{provider}_data"]["extra"]["raw_info"]
+  #       user.email = data["email"] if user.email.blank?
+  #     end
+  #   end
+  # end
 end
