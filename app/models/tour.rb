47d45a2467f2c93f0f885f02ac41cd98a93b6b00
lequan <lequan13576@gmail.com>
2022-07-26 16:08:02 +0700

    create_tour_detail

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -4,6 +4,7 @@ class Tour < ActiveRecord::Base
 	belongs_to :category
 	has_many :booktours
 	has_many :reviews
+	has_rich_text :description
 	has_attached_file :tour_img, styles: { tour_index: "250x350>", tour_show: "325x475>" }, default_url: "/images/:style/missing.png"
    	validates_attachment_content_type :tour_img, content_type: /\Aimage\/.*\z/	
 end
